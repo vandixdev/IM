bac8976745f76eb3857c5dc671fa91d3edcc1a06
BeakerB <Beaker.Bunsen@outlook.de>
2025-04-18 17:51:41 +0200

    first commit

---
+++ smaaateQuadrate.py
@@ -0,0 +1,17 @@
+import math
+
+x = []
+
+for i in range(1, 1000):
+    for j in range(i, 1000):
+        ab = math.sqrt(i*i+j*j)
+        ab -= int(ab)
+        doppelt = False
+        if ab < 0.000001:
+            for t in x:
+                if abs(i/t[0] - j/t[1]) < 0.001:
+                    doppelt = True
+                    break
+            if not doppelt:
+                x.append((i, j, math.sqrt(i*i+j*j)))
+print(x)
